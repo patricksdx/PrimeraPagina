003e19381c8ce9b7a6a028e72c0abb14e035483d
Patrick <patrick74966586@gmail.com>
2023-02-21 16:05:47 -0500

    Header responsivo1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import icono from './icons/icono1.jpg'
 
 function Header() {
   return (
-    <div className='text-white flex m-auto w-4/5' >
+    <div className='text-white flex w-full md:w-4/6 xl:w-4/5 md:m-auto' >
       <h1 className='m-4'>MARCA</h1>
       <ul className='flex ml-auto' >
         <li className='m-4'><a href='#'>Home</a></li>
