25062169b65a1c8d564e122a8399ae693671dd79
Patrick <patrick74966586@gmail.com>
2023-02-21 16:11:22 -0500

    Header responsivo2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <div className='text-white flex w-full md:w-4/6 xl:w-4/5 md:m-auto' >
       <h1 className='m-4'>MARCA</h1>
-      <ul className='flex ml-auto' >
+      <ul className='hidden ml-auto md:flex xl:flex' >
         <li className='m-4'><a href='#'>Home</a></li>
         <li className='m-4'><a href='#'>About</a></li>
         <li className='m-4'><a href='#'>Services</a></li>
