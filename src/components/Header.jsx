2001c6ecf6f30feb731e280fd2d2ee805fda73b1
Patrick <patrick74966586@gmail.com>
2023-02-21 16:39:02 -0500

    Header responsivo3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,14 @@
-import icono from './icons/icono1.jpg'
+import logo from './icons/Logo.png'
 
 function Header() {
   return (
     <div className='text-white flex w-full md:w-4/6 xl:w-4/5 md:m-auto' >
-      <h1 className='m-4'>MARCA</h1>
+      <div className='flex m-6 md:m-4'>
+      <picture>
+        <img src={logo} className='w-5 mr-4'></img>
+      </picture>
+      <h1>MARCA</h1>
+      </div>
       <ul className='hidden ml-auto md:flex xl:flex' >
         <li className='m-4'><a href='#'>Home</a></li>
         <li className='m-4'><a href='#'>About</a></li>
