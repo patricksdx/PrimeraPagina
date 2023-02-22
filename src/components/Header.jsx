e52efdf1673e31b2331798b821b0c6ed40433806
Patrick <patrick74966586@gmail.com>
2023-02-22 18:13:39 -0500

    Boton agregado3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,53 +1,61 @@
-import logo from './icons/Logo.png'
-import { FaAlignJustify } from 'react-icons/fa';
- 
+import logo from "./icons/Logo.png";
+import { FaAlignJustify } from "react-icons/fa";
+
 function Header() {
-
-  const opciones = document.querySelector('#opciones');
-  const boton = document.querySelector('#boton');
-  const boton1= document.querySelector('#boton1');
-
-  function cambiar(){
-    opciones.removeAttribute('class','hidden'),
-    boton.setAttribute('class','hidden'),
-    boton1.setAttribute('class','block');
-  }
-  function cambiar1(){
-    opciones.setAttribute('class','hidden'),
-    boton1.setAttribute('class','hidden'),
-    boton.setAttribute('class','block');
-  }
-
   return (
-    <div className='text-slate-700 grid grid-cols-2 w-full md:w-4/6 xl:w-4/5 md:m-auto' >
-      <div className='flex m-6 md:m-4'>
-      <picture>
-        <img src={logo} className='w-5 mr-4'></img>
-      </picture>
-      <h1>MARCA</h1>
+    <div className="text-slate-700 grid grid-cols-2 w-full md:w-4/6 xl:w-4/5 md:m-auto">
+      <div className="flex m-6 md:m-4">
+        <picture>
+          <img src={logo} className="w-5 mr-4"></img>
+        </picture>
+        <h1>MARCA</h1>
       </div>
-      <div className='m-6 ml-auto md:m-4 md:hidden'>
+      <div className="m-6 ml-auto md:m-4 md:hidden">
         <button
-        className='block'
-        id='boton'
-        onClick={cambiar}
+          className="block"
+          id="boton"
+          onClick={() => {
+            const opciones = document.querySelector("#opciones");
+            const boton = document.querySelector("#boton");
+            const boton1 = document.querySelector("#boton1");
+
+            opciones.removeAttribute("class"),
+              boton.setAttribute("class", "hidden"),
+              boton1.setAttribute("class", "block");
+          }}
         >
           <FaAlignJustify></FaAlignJustify>
         </button>
 
         <button
-        className='hidden'
-        id='boton1'
-        onClick={cambiar1}
+          className="hidden"
+          id="boton1"
+          onClick={() => {
+            const opciones = document.querySelector("#opciones");
+            const boton = document.querySelector("#boton");
+            const boton1 = document.querySelector("#boton1");
+
+            opciones.setAttribute("class", "hidden ml-auto md:flex xl:flex"),
+              boton1.setAttribute("class", "hidden"),
+              boton.setAttribute("class", "block");
+          }}
         >
           <FaAlignJustify></FaAlignJustify>
         </button>
       </div>
-      <ul id='opciones' className='hidden ml-auto md:flex xl:flex' >
-        <li className='w-full p-4 rounded-lg hover:text-white hover:bg-slate-700'><a href='#'>Home</a></li>
-        <li className='w-full p-4 rounded-lg hover:text-white hover:bg-slate-700'><a href='#'>About</a></li>
-        <li className='w-full p-4 rounded-lg hover:text-white hover:bg-slate-700'><a href='#'>Services</a></li>
-        <li className='w-full p-4 rounded-lg hover:text-white hover:bg-slate-700'><a href='#'>Contact</a></li>
+      <ul id="opciones" className="hidden ml-auto md:flex xl:flex">
+        <li className="w-full p-4 rounded-lg hover:text-white hover:bg-slate-700">
+          <a href="#">Home</a>
+        </li>
+        <li className="w-full p-4 rounded-lg hover:text-white hover:bg-slate-700">
+          <a href="#">About</a>
+        </li>
+        <li className="w-full p-4 rounded-lg hover:text-white hover:bg-slate-700">
+          <a href="#">Services</a>
+        </li>
+        <li className="w-full p-4 rounded-lg hover:text-white hover:bg-slate-700">
+          <a href="#">Contact</a>
+        </li>
       </ul>
     </div>
   );
