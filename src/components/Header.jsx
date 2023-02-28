d0bf138aba30dcaa32355c9102d3801f68da480e
Patrick <patrick74966586@gmail.com>
2023-02-27 20:13:12 -0500

    navegador responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import logo from "./icons/Logo.png";
 import { FaAlignJustify } from "react-icons/fa";
+import { BsXLg } from "react-icons/bs";
 
 function Header() {
   return (
-    <div className="text-slate-700 grid grid-cols-2 w-full md:w-4/6 xl:w-4/5 md:m-auto">
+    <header className="text-slate-700 grid grid-cols-2 w-full md:w-4/6 xl:w-4/5 md:m-auto">
       <div className="flex m-6 md:m-4">
         <picture>
           <img src={logo} className="w-5 mr-4"></img>
@@ -16,21 +17,28 @@ function Header() {
           id="boton"
           onClick={() => {
             const opciones = document.querySelector("#opciones");
-            const navegacion =document.querySelector("#navegacion");
-            const navegacion1 =document.querySelector("#navegacion1");
-            const navegacion2 =document.querySelector("#navegacion2");
-            const navegacion3 =document.querySelector("#navegacion3");
+            const navegacion = document.querySelector("#navegacion");
+            const navegacion1 = document.querySelector("#navegacion1");
+            const navegacion2 = document.querySelector("#navegacion2");
+            const navegacion3 = document.querySelector("#navegacion3");
             const boton = document.querySelector("#boton");
-            const boton1 = document.querySelector("#boton1");
+            const boton1 = document.querySelector("#boton1"); 
+            const livnav2 = document.querySelector("#livnav2");
+            const nav2 = document.querySelector('#nav2');
 
             opciones.removeAttribute("class"),
-              opciones.setAttribute("class","col-start-1 col-end-3 text-center"),
-              navegacion.setAttribute("class","px-24 py-2"),
-              navegacion1.setAttribute("class","px-24 py-2"),
-              navegacion2.setAttribute("class","px-24 py-2"),
-              navegacion3.setAttribute("class","px-24 py-2"),
+              opciones.setAttribute(
+                "class",
+                "col-start-1 col-end-3 text-center"
+              ),
+              navegacion.setAttribute("class", "px-24 py-2 block"),
+              navegacion1.setAttribute("class", "px-24 py-2 block"),
+              navegacion2.setAttribute("class", "px-24 py-2 block"),
+              navegacion3.setAttribute("class", "px-24 py-2 block"),
               boton.setAttribute("class", "hidden"),
-              boton1.setAttribute("class", "block");
+              boton1.setAttribute("class", "block"),
+              nav2.setAttribute("class","hidden block z-10 group-hover:visible group-hover:block"),
+              livnav2.setAttribute("class","group");
           }}
         >
           <FaAlignJustify></FaAlignJustify>
@@ -49,24 +57,49 @@ function Header() {
               boton.setAttribute("class", "block");
           }}
         >
-          <FaAlignJustify></FaAlignJustify>
+          <BsXLg></BsXLg>
         </button>
       </div>
-      <ul id="opciones" className="hidden ml-auto md:flex xl:flex col-end">
-        <li className="w-full p-4 rounded-lg hover:text-white hover:bg-slate-700">
-          <a id='navegacion' href="#">Home</a>
+      <ul id="opciones" className="hidden ml-auto mr-0 my-auto md:flex xl:flex col-end">
+        <li>
+          <a id="navegacion" href="#" className="block px-6 py-1 hover:text-white hover:bg-slate-700">
+            Home
+          </a>
         </li>
-        <li className="w-full p-4 rounded-lg hover:text-white hover:bg-slate-700">
-          <a id='navegacion1' href="#">About</a>
+        <li>
+          <a id="navegacion1" href="#" className="block px-6 py-1 hover:text-white hover:bg-slate-700">
+            About
+          </a>
         </li>
-        <li className="w-full p-4 rounded-lg hover:text-white hover:bg-slate-700">
-          <a id='navegacion2' href="#">Services</a>
+        <li id="linav2" className="group" >
+          <a id="navegacion2" href="#" className="block  px-6 py-1 hover:text-white hover:bg-slate-700">
+            Services
+          </a>
+          <ul id="nav2" className="group-hover:visible invisible absolute z-10">
+            <li>
+              <a href="#" className="block px-6 py-1 hover:text-white hover:bg-slate-700">
+              Service 1
+              </a>
+            </li>
+            <li>
+              <a href="#" className="block px-6 py-1 hover:text-white hover:bg-slate-700">
+              Service 2
+              </a>
+            </li>
+            <li>
+              <a href="#" className="block px-6 py-1 hover:text-white hover:bg-slate-700">
+              Service 3
+              </a>
+            </li>
+          </ul>
         </li>
-        <li className="w-full p-4 rounded-lg hover:text-white hover:bg-slate-700">
-          <a id='navegacion3' href="#">Contact</a>
+        <li >
+          <a id="navegacion3" href="#" className="block px-6 py-1 hover:text-white hover:bg-slate-700">
+            Contact
+          </a>
         </li>
       </ul>
-    </div>
+    </header>
   );
 }
 
